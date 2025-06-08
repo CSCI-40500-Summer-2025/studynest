bc63419615bcca9a507be8336a2391d0c8c653ce
Maya Tabbah <mayatabbah7@gmail.com>
2025-06-08 18:23:27 -0400

    initialized vite project

---
+++ studynest/src/Layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState } from "react";
+
+const Layout = () => {
+  return (
+    <div>
+      <h1>Welcome to StudyNest</h1>
+      <p>Making your academic dreams come true?</p>
+    </div>
+  );
+};
+
+export default Layout;
--- studynest/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div>
-      <h1>StudyName App</h1>
-    </div>
-  );
-}
